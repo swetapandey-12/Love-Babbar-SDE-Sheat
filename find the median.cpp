b16be4815cf833611e7f9b466a4d6c804be02ee9
sweta pandey <74544867+swetapandey-12@users.noreply.github.com>
2022-10-10 18:45:28 +0530

    Create find the median.cpp

---
+++ find the median.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+	public:
+		int find_median(vector<int> v)
+		{
+		    // Code here.
+	    	sort(v.begin(),v.end());
+            int n = v.size();
+            if(n%2 !=0){
+                 return v[floor(n/2)];
+            }
+            else
+            {
+                return ((v[floor(n/2)]+v[floor(n/2)-1])/2);
+            }
+   }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>v[i];
+    	Solution ob;
+    	int ans = ob.find_median(v);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
