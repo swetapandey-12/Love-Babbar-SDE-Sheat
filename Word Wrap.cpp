61b53cb0a6bae91bb637966268e5a6031fd05f60
sweta pandey <74544867+swetapandey-12@users.noreply.github.com>
2022-10-31 23:23:55 +0530

    Create Word Wrap.cpp

---
+++ Word Wrap.cpp
@@ -0,0 +1,29 @@
+int n=nums.size();
+        vector<int> dp(n,INT_MAX);
+        int i,j;
+        int curr,cost;
+        dp[n-1]=0;
+        for(i=n-2;i>=0;i--)
+        {
+            curr=-1;
+            for(j=i;j<n;j++)
+            {
+                curr+=nums[j]+1;
+                if(curr>k)
+                {
+                    break;
+                }
+                if(j==n-1)
+                {
+                    cost=0;
+                }
+                else
+                {
+                    cost=(k-curr)*(k-curr)+dp[j+1];
+                }
+                dp[i]=min(dp[i],cost);
+            }
+        }
+        return dp[0];
+    } 
+};
