e91d597b13ee3c213b5961bb6cf2492c3e02ea64
sweta pandey <74544867+swetapandey-12@users.noreply.github.com>
2023-09-12 12:55:48 +0530

    Create minimum and maximum.cpp

---
+++ array/minimum and maximum.cpp
@@ -0,0 +1,15 @@
+pair<long long, long long> getMinMax(long long a[], int n) {
+    long long min =INT_MAX;
+    long long max =INT_MIN;
+    for(int i=0;i<n;i++){
+        if(a[i]>max){
+            max=a[i];
+        }
+        if(a[i]<min){
+            min = a[i];
+        }
+    }
+    return {min, max};
+    
+    
+}
